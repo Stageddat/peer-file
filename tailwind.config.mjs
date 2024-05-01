7a9f5082240078703ee9cb3de518ff1aaa37d0b3
Stageddat <chmarc72@gmail.com>
2024-05-01 17:20:15 +0200

    Add animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				"slide-down": "slideDown 1s ease-in-out",
+				"none": "none",
+				"slide-in-top": "slideInTop 0.6s ease-out both"
+			},
+		},
+		keyframes: {
+			slideDown: {
+				"0%": { transform: "translateY(-100%)", opacity: 0 },
+				"100%": { transform: "translateY(0)", opacity: 1 },
+			},
+			slideInTop: {
+				"0%": { transform: "translateY(-20px)" },
+				"100%": { transform: "translateY(0)" }
+			},
+		},
 	},
 	plugins: [],
 }
